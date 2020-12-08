0812ca815a9411d9d7e2a72e4f09f6e31d5b3bb9
ChaosMarc <chaosmarc@gmx.de>
2020-12-08 16:17:04 +0100

    2020 Day 8 refactoring

--- src/de/chaosmarc/aoc/twentytwenty/Day8Part1.java
+++ src/de/chaosmarc/aoc/twentytwenty/Day8Part1.java
@@ -31,17 +31,13 @@ public class Day8Part1 {
             }
             pastInstructions.add(ptr);
             String[] instruction = instructions.get(ptr).split(" ");
-            switch (instruction[0]) {
-                case "acc":
-                    acc += Integer.parseInt(instruction[1]);
-                    ptr++;
-                    break;
-                case "nop":
-                    ptr++;
-                    break;
-                case "jmp":
-                    ptr += Integer.parseInt(instruction[1]);
-                    break;
+            if ("acc".equals(instruction[0])) {
+                acc += Integer.parseInt(instruction[1]);
+                ptr++;
+            } else if ("nop".equals(instruction[0])) {
+                ptr++;
+            } else if ("jmp".equals(instruction[0])) {
+                ptr += Integer.parseInt(instruction[1]);
             }
             if (ptr == instructions.size() - 1) {
                 System.out.println("Solution: " + acc);
--- src/de/chaosmarc/aoc/twentytwenty/Day8Part2.java
+++ src/de/chaosmarc/aoc/twentytwenty/Day8Part2.java
@@ -7,21 +7,16 @@ public class Day8Part2 {
     public static void main(String[] args) throws IOException {
         List<String> instructions = Day8Part1.readInput();
         for (int i = 0; i < instructions.size(); i++) {
-            if (instructions.get(i).matches("(jmp|nop).*") && runWithSwappedInstruction(instructions, i)) {
-                return;
+            String instruction = instructions.get(i);
+            if (instruction.matches("(jmp|nop).*")) {
+                instructions.set(
+                    i, instruction.startsWith("nop") ? instruction.replace("nop", "jmp") :
+                        instruction.replace("jmp", "nop"));
+                if (Day8Part1.run(instructions)) {
+                    return;
+                }
+                instructions.set(i, instruction);
             }
         }
-    }
-
-    private static boolean runWithSwappedInstruction(List<String> instructions, int i) {
-        String instruction = instructions.get(i);
-        if (instruction.startsWith("nop")) {
-            instructions.set(i, instruction.replace("nop", "jmp"));
-        } else {
-            instructions.set(i, instruction.replace("jmp", "nop"));
-        }
-        boolean result = Day8Part1.run(instructions);
-        instructions.set(i, instruction);
-        return result;
     }
 }
