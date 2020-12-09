94352506d74871c92c7844ecfbe416876d6ecfa4
ChaosMarc <chaosmarc@gmx.de>
2020-12-09 08:05:18 +0100

    2020 Day 9 (using refactored 2020 Day 1)

---
+++ src/de/chaosmarc/aoc/twentytwenty/Day9Part1.java
@@ -0,0 +1,25 @@
+package de.chaosmarc.aoc.twentytwenty;
+
+import de.chaosmarc.aoc.Helper;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day9Part1 {
+
+    public static void main(String[] args) throws IOException {
+        List<Long> input = Helper.readInput(2020, 9).stream().map(Long::valueOf).collect(Collectors.toList());
+        System.out.println("Solution: " + findNumber(input));
+    }
+
+    public static long findNumber(List<Long> input) {
+        for (int i = 25; i < input.size(); i++) {
+            long[] twoNumbers = Day1Part1.findTwoNumbers(input.subList(i - 25, i), input.get(i));
+            if (twoNumbers.length != 2) {
+                return input.get(i);
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/de/chaosmarc/aoc/twentytwenty/Day9Part2.java
@@ -0,0 +1,44 @@
+package de.chaosmarc.aoc.twentytwenty;
+
+import de.chaosmarc.aoc.Helper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day9Part2 {
+
+    public static void main(String[] args) throws IOException {
+        List<Long> input = Helper.readInput(2020, 9).stream().map(Long::valueOf).collect(Collectors.toList());
+        List<Long> summands = findSummands(input, Day9Part1.findNumber(input));
+        System.out.println("Solution: " + (Collections.min(summands) + Collections.max(summands)));
+    }
+
+    public static List<Long> findSummands(List<Long> input, long number) {
+        ArrayList<Long> summands = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            summands.add(input.get(i));
+            for (int j = i + 1; j < input.size(); j++) {
+                summands.add(input.get(j));
+                long sum = getSum(summands);
+                if (sum == number) {
+                    return summands;
+                } else if (sum > number) {
+                    summands.clear();
+                    break;
+                }
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    public static long getSum(List<Long> summands) {
+        long sum = 0;
+        for (Long summand : summands) {
+            sum += summand;
+        }
+        return sum;
+    }
+}
