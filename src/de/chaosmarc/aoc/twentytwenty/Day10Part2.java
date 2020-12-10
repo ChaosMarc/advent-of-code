a212db9148fed3d28605c72e3ef8395a8bfae07e
ChaosMarc <chaosmarc@gmx.de>
2020-12-10 14:24:27 +0100

    2020 Day 10 Part 2

--- src/de/chaosmarc/aoc/twentytwenty/Day10Part2.java
+++ src/de/chaosmarc/aoc/twentytwenty/Day10Part2.java
@@ -6,28 +6,42 @@ import java.util.List;
 import java.util.Map;
 
 public class Day10Part2 {
+
+    private static final Map<Integer, Integer> patternValues = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         List<Integer> input = Day10Part1.getModifiedInput();
-
-        // per block of 1s there are only so many combinations possible to remove some of them
-        Map<Integer, Integer> patternValues = new HashMap<>();
-        patternValues.put(0, 1);
-        patternValues.put(1, 1);
-        patternValues.put(2, 2); // Diff-Combinations: 1,1 + 2
-        patternValues.put(3, 4); // Diff-Combinations: 1,1,1 + 2,1 + 1,2 + 3
-        patternValues.put(4, 7); // Diff-Combinations: 1,1,1,1 + 2,1,1 + 1,2,1 + 1,1,2 + 2,2 + 3,1 + 1,3
-
         int oneCount = 0;
         long combinations = 1;
         for (int i = 1; i < input.size(); i++) {
             if (input.get(i) - input.get(i - 1) == 1) {
                 oneCount++;
             } else {
-                combinations *= patternValues.get(oneCount);
+                combinations *= getPossibleCombinationCount(oneCount);
                 oneCount = 0;
             }
         }
         System.out.println("Solution: " + combinations);
+    }
+
+    private static int getPossibleCombinationCount(int count) {
+        // per block of 1s there are only so many combinations possible when removing some of them
+        // block of 2x 1s = 1,1 + 2 (2)
+        // block of 3x 1s = 1,1,1 + 2,1 + 1,2 + 3 (4)
+        // block of 4x 1s = 1,1,1,1 + 2,1,1 + 1,2,1 + 1,1,2 + 2,2 + 3,1 + 1,3 (7)
+        // which are the Tribonacci Numbers
+        if (!patternValues.containsKey(count)) {
+            if (count <= 1) {
+                patternValues.put(count, 1);
+            } else if (count == 2) {
+                patternValues.put(count, 2);
+            } else {
+                patternValues.put(
+                    count, getPossibleCombinationCount(count - 1) + getPossibleCombinationCount(count - 2)
+                        + getPossibleCombinationCount(count - 3));
+            }
+        }
+        return patternValues.get(count);
     }
 }
 
