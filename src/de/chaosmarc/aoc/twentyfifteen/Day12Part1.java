5236ad5d31dd9d17c22dff28e58d3967e9ebd2b0
ChaosMarc <chaosmarc@gmx.de>
2020-12-15 18:06:26 +0100

    2015 Day 12 Part 1

---
+++ src/de/chaosmarc/aoc/twentyfifteen/Day12Part1.java
@@ -0,0 +1,14 @@
+package de.chaosmarc.aoc.twentyfifteen;
+
+import de.chaosmarc.aoc.Helper;
+
+import java.io.IOException;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+public class Day12Part1 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Solution: " + Pattern.compile("-?\\d+").matcher(Helper.readInput(2015, 12).get(0)).results()
+            .map(MatchResult::group).mapToInt(Integer::parseInt).reduce(0, Integer::sum));
+    }
+}
