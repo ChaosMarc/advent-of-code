261e16703d751e5c4d19ad9e55a90bde60055b87
ChaosMarc <chaosmarc@gmx.de>
2020-12-16 21:46:38 +0100

    2020 Day 16 Part 2 (streamified, still ugly :D)

--- src/de/chaosmarc/aoc/twentytwenty/Day16Part2.java
+++ src/de/chaosmarc/aoc/twentytwenty/Day16Part2.java
@@ -12,8 +12,6 @@ public class Day16Part2 {
         List<List<Integer>> tickets = new ArrayList<>();
         List<List<Integer>> rules = new ArrayList<>();
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
         for (String line : input) {
             if (line.contains(":") || line.equals("")) {
                 String[] split = line.split("[\\- ]");
@@ -24,62 +22,32 @@ public class Day16Part2 {
                     rule.add(Integer.parseInt(split[split.length - 2]));
                     rule.add(Integer.parseInt(split[split.length - 1]));
                     rules.add(rule);
-                    min = Math.min(min, Integer.parseInt(split[split.length - 5]));
-                    max = Math.max(max, Integer.parseInt(split[split.length - 1]));
                 }
             } else {
                 tickets.add(Arrays.stream(line.split(",")).map(Integer::parseInt).collect(Collectors.toList()));
             }
         }
 
-        List<List<Integer>> validTickets = new ArrayList<>();
-        for (List<Integer> ticket : tickets) {
-            boolean isValid = true;
-            for (int field : ticket) {
-                if (field < min || field > max) {
-                    isValid = false;
-                    break;
-                }
-            }
-            if (isValid) {
-                validTickets.add(ticket);
-            }
-        }
+        IntSummaryStatistics stats = rules.stream().flatMap(List::stream).collect(Collectors.toList()).stream()
+            .collect(Collectors.summarizingInt(Integer::intValue));
+        List<List<Integer>> validTickets =
+            tickets.stream().filter(t -> t.stream().noneMatch(f -> f < stats.getMin() || f > stats.getMax()))
+                .collect(Collectors.toList());
 
-        int fieldCount = tickets.get(0).size();
         Map<Integer, List<Integer>> finalRuleMapping = new HashMap<>(); // rulePos, fieldPos
-        while (finalRuleMapping.size() < fieldCount) {
+        while (finalRuleMapping.size() < tickets.get(0).size()) {
             Map<Integer, List<Integer>> ruleMapping = new HashMap<>();
-            for (int i = 0; i < rules.size(); i++) {
-                List<Integer> rule = rules.get(i);
-                if (finalRuleMapping.containsKey(i)) {
-                    continue;
-                }
-                for (int j = 0; j < fieldCount; j++) {
-                    boolean skip = false;
-                    for (Map.Entry<Integer, List<Integer>> entry : finalRuleMapping.entrySet()) {
-                        if (entry.getValue().get(0) == j) {
-                            skip = true;
-                            break;
-                        }
-                    }
-                    if (skip) {
-                        continue;
-                    }
-
-                    boolean ruleMatchesAllFields = true;
-                    for (List<Integer> ticket : validTickets) {
-                        Integer field = ticket.get(j);
-                        if (!((field >= rule.get(0) && field <= rule.get(1)) || (field >= rule.get(2) && field <= rule
-                            .get(3)))) {
-                            ruleMatchesAllFields = false;
-                            break;
-                        }
-                    }
-                    if (ruleMatchesAllFields) {
-                        List<Integer> rm = ruleMapping.containsKey(i) ? ruleMapping.get(i) : new ArrayList<>();
+            for (int i1 = 0; i1 < rules.size(); i1++) {
+                int i = i1;
+                for (int j1 = 0; j1 < tickets.get(0).size(); j1++) {
+                    int j = j1;
+                    if (finalRuleMapping.values().stream().noneMatch(x -> x.get(0) == j) && validTickets.stream()
+                        .allMatch(
+                            ticket -> (ticket.get(j) >= rules.get(i).get(0) && ticket.get(j) <= rules.get(i).get(1))
+                                || (ticket.get(j) >= rules.get(i).get(2) && ticket.get(j) <= rules.get(i).get(3)))) {
+                        List<Integer> rm = ruleMapping.containsKey(i1) ? ruleMapping.get(i1) : new ArrayList<>();
                         rm.add(j);
-                        ruleMapping.put(i, rm);
+                        ruleMapping.put(i1, rm);
                     }
                 }
             }
